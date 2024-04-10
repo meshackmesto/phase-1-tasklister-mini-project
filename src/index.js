ab830adb4e34586f755edf463909ab957fc79326
meshackmesto <meshackorina93@gmail.com>
2024-04-10 16:27:59 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+let form = document.querySelector("form")
+form.addEventListener("submit", () => {
+  e.preventDefault();
+  todo(e.target.new-task-description.value);
+  form.reset();
+})
+ 
 });
+
+function todo(dotask){
+  let p = document.createElement("p")
+  let btn = document.createElement("button");
+  btn.textContent = "Delete";
+  p.textContent = `${dotask}`;
+  console.log(p);
+  document.querySelector('#tasks').appendChild(p);
+}
+function deleting(e){
+  e.target.parentNode.remove();
+}
